ca22fda8e0e26ff8fdd5fab424b9c4ebf408845b
shoebsmk <your_email_address@example.com>
2022-09-16 23:28:12 -0500

    Hi

---
+++ app/src/main/java/com/christopherhield/classintroapp/MainActivity.java
@@ -0,0 +1,36 @@
+package com.christopherhield.classintroapp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello Wierd World");
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+    public void countUp(View view) {
+        mCount++;
+        if(mShowCount != null){
+            mShowCount.setText(Integer.toString(mCount));
+        }
+    }
+}
